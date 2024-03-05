6454dd593bf6a140bc1b395bc2e23d16557e83e4
beccap123 <pratel.1@osu.edu>
2024-03-05 12:03:16 -0500

    uhhhh not sure joined late tuesday, no lunch yet

--- src/week1/TodosSolution/TodosApi/Program.cs
+++ src/week1/TodosSolution/TodosApi/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.AspNetCore.Mvc;
+using TodosApi;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -14,10 +17,33 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+// GET /status
 app.MapGet("/status", () =>
 {
     return Results.Ok();
 });
 
-app.Run();
+app.MapPost("/todos", ([FromBody] TodoCreateRequest request) =>
+{
+    // look at the content they sent and validate it
+    // if it's not "valid", send thema 400 response
+    // if it is valid, 
+    //   - Assign an ID to it
+    //   - Save it somewhere (database?)
+    //   - Send it back to them
 
+    //send fake response
+    var response = new TodoCreateResponse
+    {
+        Id = Guid.NewGuid(),
+        Description = request.Description,
+        Status = TodoStatus.Incomplete
+    };
+    return Results.Ok(response);
+});
+
+//Console.WriteLine("Fixin' to start the server");
+app.Run(); // starts server and just listens for requests
+//Console.WriteLine("Done running the server");
+
+public partial class Program { }
---
+++ src/week1/TodosSolution/TodosApiContractTests/StatusResource/GettingTheStatus.cs
@@ -0,0 +1,33 @@
+﻿
+using Alba;
+
+namespace TodosApi.ContractTests.StatusResource;
+public class GettingTheStatus
+{
+
+    [Fact]
+
+    public async Task ReturnsOk()
+    {
+        /*
+        // Given
+        var client = new HttpClient();
+        client.BaseAddress = new Uri("http://localhost:1337");
+
+        // When
+        var response = await client.GetAsync("/status");
+
+        // Then
+        Assert.Equal(System.Net.HttpStatusCode.OK, response.StatusCode);
+        */
+
+
+        var host = await AlbaHost.For<Program>();
+
+        var response = await host.Scenario(api =>
+        {
+            api.Get.Url("/status");
+            api.StatusCodeShouldBeOk();
+        });
+    }
+}
