7e463d13d8937f5763d494d1a40403955e84e5f9
beccap123 <pratel.1@osu.edu>
2024-03-05 13:46:54 -0500

    RIGHT before lunch tuesday

--- src/week1/TodosSolution/TodosApi/Program.cs
+++ src/week1/TodosSolution/TodosApi/Program.cs
@@ -1,3 +1,4 @@
+using Marten;
 using Microsoft.AspNetCore.Mvc;
 using TodosApi;
 
@@ -7,6 +8,14 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var connectionString = builder.Configuration.GetConnectionString("todos") ??
+    throw new Exception("Can't start, need a connection string");
+
+builder.Services.AddMarten(options =>
+{
+    options.Connection(connectionString);
+}).UseLightweightSessions();
 
 var app = builder.Build();
 
@@ -23,7 +32,8 @@ app.MapGet("/status", () =>
     return Results.Ok();
 });
 
-app.MapPost("/todos", ([FromBody] TodoCreateRequest request) =>
+app.MapPost("/todos", async ([FromBody] TodoCreateRequest request,
+    [FromServices] IDocumentSession session) =>
 {
     // look at the content they sent and validate it
     // if it's not "valid", send thema 400 response
@@ -39,7 +49,18 @@ app.MapPost("/todos", ([FromBody] TodoCreateRequest request) =>
         Description = request.Description,
         Status = TodoStatus.Incomplete
     };
+    // tell it what you want it to do
+    session.Store(response);
+    // actually write it to the database
+    await session.SaveChangesAsync();
+
     return Results.Ok(response);
+});
+
+app.MapGet("/todos", async ([FromServices] IDocumentSession session) =>
+{
+    var todoList = await session.Query<TodoCreateResponse>().ToListAsync();
+    return Results.Ok(todoList);
 });
 
 //Console.WriteLine("Fixin' to start the server");
